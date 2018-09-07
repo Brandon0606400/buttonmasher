385898bf081069ff227c98a512ca8590389d0848
Brandon0606400 <43067373+Brandon0606400@users.noreply.github.com>
2018-09-07 16:45:16 +0100

    Added debug messages

---
+++ Assets/Scripts/buttonMasher.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttonMasher : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Start method called");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log("Start method called 2");
+    }
+}
