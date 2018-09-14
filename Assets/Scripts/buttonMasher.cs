5e774370de84296b181c724ec3629d17d51b9fe6
Brandon0606400 <43067373+Brandon0606400@users.noreply.github.com>
2018-09-14 16:01:11 +0100

    Added a display message and score reset
    Adds a display message and shows score at the end. resets the score when the game ends.

--- Assets/Scripts/buttonMasher.cs
+++ Assets/Scripts/buttonMasher.cs
@@ -9,16 +9,17 @@ public class buttonMasher : MonoBehaviour {
     public TextMesh scoreText; // display text for the player's score
     public TextMesh timeText; // display text for the player's time
     public float gameLength; // how many seconds the game will last
+    public AudioSource gameOverSound; // sound that will play when it game overs
+    public TextMesh messageText; // display text for the player's time
 
     private int score = 0; // The numerical data value of the score
     private float timeRemaining = 0; // The numerical data value of the time
-    private bool gameRunning = true;
+    private bool gameRunning = false;
 
 	// Use this for initialization
 	void Start () {
         Debug.Log("Start method called");
 
-        timeRemaining = gameLength;
 	} // End of start
 	
 	// Update is called once per frame
@@ -34,6 +35,19 @@ public class buttonMasher : MonoBehaviour {
         // Check to see if we have run out of time
         if (timeRemaining <= 0)
         {
+
+            if (gameRunning == true)
+            {
+                Debug.Log("Attempting to play game over");
+
+                // play the game over sound
+                gameOverSound.Play();
+
+                //show the player their score
+                messageText.text = "Time up! Final Score = " + score.ToString();
+            } // End of (gameRunning == true)
+
+            //stop the game running
             gameRunning = false;
 
             //Stop out game from going negative
@@ -57,7 +71,20 @@ public class buttonMasher : MonoBehaviour {
             // update visual score
             scoreText.text = score.ToString();
         } //End of if(gameRunning == true)
-        
+        else
+        {
+            //game is not running and we click the button
+            gameRunning = true;
+
+            //Set the timer to the full length of the game
+            timeRemaining = gameLength;
+
+            messageText.text = "Mash the button!";
+
+            //reset the score
+            score = 0;
+
+        } //End of game else
     } // End of OnMouseDown
 
 } // End of buttonMasher class
