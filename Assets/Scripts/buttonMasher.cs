9c45d702f619a1acb92e2c7da59e1849ec922089
Brandon0606400 <43067373+Brandon0606400@users.noreply.github.com>
2018-09-14 15:29:02 +0100

    Added Additional sounds and Added timer
    We replaced sounds and added a timer countdown from 10 and then stop the game

--- Assets/Scripts/buttonMasher.cs
+++ Assets/Scripts/buttonMasher.cs
@@ -7,30 +7,57 @@ public class buttonMasher : MonoBehaviour {
     // Data for our class
     public AudioSource clickSound; // sound that will play when we click
     public TextMesh scoreText; // display text for the player's score
+    public TextMesh timeText; // display text for the player's time
+    public float gameLength; // how many seconds the game will last
 
     private int score = 0; // The numerical data value of the score
+    private float timeRemaining = 0; // The numerical data value of the time
+    private bool gameRunning = true;
 
 	// Use this for initialization
 	void Start () {
         Debug.Log("Start method called");
 
+        timeRemaining = gameLength;
 	} // End of start
 	
 	// Update is called once per frame
 	void Update () {
         Debug.Log("Update method called");
+
+        // update the numerical time remaining
+        timeRemaining = timeRemaining - Time.deltaTime;
+
+        // update the visual time remaining
+        timeText.text = (Mathf.CeilToInt(timeRemaining)).ToString();
+
+        // Check to see if we have run out of time
+        if (timeRemaining <= 0)
+        {
+            gameRunning = false;
+
+            //Stop out game from going negative
+            timeRemaining = 0;
+        } // end of if timeRunning
+
     } // End of update
 
     // Responds to event from unity that the object has been clicked
     void OnMouseDown()
     {
         Debug.Log("OnMouseDown method called");
-        // Trigger the clicking sound to play
-        clickSound.Play();
-        // increase the score by 1
-        score = score + 1;
-        // update visual score
-        scoreText.text = score.ToString();
+
+        //Check to see if game is still running
+        if(gameRunning == true)
+        {
+            // Trigger the clicking sound to play
+            clickSound.Play();
+            // increase the score by 1
+            score = score + 1;
+            // update visual score
+            scoreText.text = score.ToString();
+        } //End of if(gameRunning == true)
+        
     } // End of OnMouseDown
 
 } // End of buttonMasher class
